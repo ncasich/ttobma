0ecb95e37f95c9cd92d05453457bb501edcd7456
nick.casich <nickolay.casich@gmail.com>
2024-01-26 09:58:01 +0100

    vite working

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,41 @@
-import { defineConfig } from 'vite';
+import {defineConfig, loadEnv} from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
-});
+
+export default ({mode}) => {
+    process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+    return defineConfig({
+        server: {
+            host: '0.0.0.0',
+            port: 5173,
+            hmr: {
+                port: 5173,
+                clientPort: 5173,
+                host: 'localhost'
+            }
+        },
+        plugins: [
+            laravel({
+                input: [
+                    'resources/sass/app.scss',
+                    'resources/js/app.js',
+                ],
+                refresh: true,
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
+                },
+            }),
+        ],
+        resolve: {
+            alias: {
+                vue: 'vue/dist/vue.esm-bundler.js',
+            },
+        },
+    });
+}
